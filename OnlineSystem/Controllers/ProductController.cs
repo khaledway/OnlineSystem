d4d06124b77f0e9f5bdd05d39c4c34b71290aa14
khaled hussein <khaled.hussein@57357.org>
2022-12-03 19:33:58 +0200

    1-add nuget for nlog 2-add nlog and configure it in : Program setting add new file called nlog.config

--- OnlineSystem/Controllers/ProductController.cs
+++ OnlineSystem/Controllers/ProductController.cs
@@ -6,13 +6,18 @@ namespace OnlineSystem.Controllers
     public class ProductController : Controller
     {
         private IProductService _IProductService { get; set; }
-        public ProductController(IProductService IProductService)
+
+       
+        private readonly ILogger<ProductController> _ILogger;
+
+        public ProductController(IProductService IProductService, ILogger<ProductController> iLogger)
         {
             _IProductService = IProductService;
-
+            _ILogger = iLogger;
         }
         public IActionResult Index()
         {
+
             var product = _IProductService.GetProductList();
             return View();
         }
--- OnlineSystem/Program.cs
+++ OnlineSystem/Program.cs
@@ -6,45 +6,73 @@ using OnlineSystem.Infrastructure.DataBase;
 using Microsoft.EntityFrameworkCore;
 using OnlineSystem.Infrastructure;
 using Microsoft.AspNetCore.SignalR;
+using NLog.Web;
+using NLog;
+using NLog.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+
+
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+try
+{
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-
-#region My Service
-
-builder.Services.AddDbContext<OnlineShopContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
-});
-builder.Services.AddScoped<IOnlineShopContext, OnlineShopContext>();
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-#endregion
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
 
+    #region My Service
 
-var app = builder.Build();
+    builder.Services.AddDbContext<OnlineShopContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection"));
+    });
+    builder.Services.AddScoped<IOnlineShopContext, OnlineShopContext>();
+    builder.Services.AddScoped<IProductService, ProductService>();
+    builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+    #endregion
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+
+    #region my app configuration
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+    #endregion
+
+
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+   
+
+    app.Run();
+
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception ex)
+{
+    logger.Error(ex , "test In program : Stopped program because of exception");
+}
+finally 
+{
+    NLog.LogManager.Shutdown();
+}
